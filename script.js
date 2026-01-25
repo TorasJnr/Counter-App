154c90367ca12f407ec6b6890a1470fae26919f7
TorasJnr <rasheedidris416@gmail.com>
2026-01-25 23:30:12 +0100

    Initial commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', ()=> {
     const counterValueE1 = document.querySelector('[data-counter-value]');
     const addBtn = document.querySelector('[data-add]');
+    const subBtn = document.querySelector('data-sub');
 })
 
 // git add .
