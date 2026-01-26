7741644b73607830829be8c07f0c0209d3308ac5
TorasJnr <rasheedidris416@gmail.com>
2026-01-26 23:46:13 +0100

    Initial commit

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 document.addEventListener('DOMContentLoaded', ()=> {
-    const counterValueE1 = document.querySelector("counter-value");
+    const counter = document.querySelector("counter-value");
     const addBtn = document.querySelector("add-btn");
     const subBtn = document.querySelector("sub-btn");
     const resetBtn = document.querySelector("reset-btn");
     function handleAdd() {
-        let currentvalue = Number(counterValueE1.textContent);
+        let currentvalue = Number(counter.textContent);
         currentvalue += 1;
-        counterValueE1.textContent = currentvalue;
+        counter.textContent = currentvalue;
     }
     function handleSubtract() {
-        let currentvalue = Number(counterValueEl.textContent);
+        let currentvalue = Number(counter.textContent);
         if (currentvalue > 0) {
             currentvalue -= 1;
-            counterValueEl.textContent = currentvalue;
+            counter.textContent = currentvalue;
         }
     }
 
