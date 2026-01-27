5b8112de2d1102b1679451c049c0d9f9a62c309e
TorasJnr <rasheedidris416@gmail.com>
2026-01-27 23:31:53 +0100

    Initial commit

--- script.js
+++ script.js
@@ -21,20 +21,10 @@ document.addEventListener('DOMContentLoaded', ()=> {
     }
 
 addBtn.addEventListener('click', handleAdd);
+
 subBtn.addEventListener('click', handleSubtract);
 
 resetBtn.addEventListener('click', handleReset);
-
-
-
-
-
-
-
-
-
-
-
 
 
 });
